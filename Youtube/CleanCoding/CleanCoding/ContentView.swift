dc53a1f421fe36cd6b241ae81cf9d95248596719
maksbond <bondarmaksymwork@gmail.com>
2019-10-09 21:47:09 +0300

    Created first view with buttons, custom viewModifier and used @State for default values.

---
+++ Youtube/CleanCoding/CleanCoding/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  CleanCoding
+//
+//  Created by Maksym Bondar on 10/8/19.
+//  Copyright © 2019 Maksym Bondar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack() {
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Text("Continue")
+                        .modifier(CustomModifier())
+                })
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("More Details")
+                    .modifier(CustomModifier(fontSize: 10, backgroundColor: .green))
+                })
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Cancel")
+                        .modifier(CustomModifier(backgroundColor: .red))
+                })
+            }
+        }
+    }
+}
+
+struct CustomModifier: ViewModifier {
+    @State var fontSize: CGFloat = 16.0
+    @State var backgroundColor = Color.blue
+    
+    func body(content: Content) -> some View {
+        return content
+            .foregroundColor(.white)
+            .font(.system(size: fontSize))
+            .padding(.horizontal, 14)
+            .padding(.vertical, 10)
+            .background(backgroundColor)
+            .overlay(
+                RoundedRectangle(cornerRadius: 3)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 1))
+                    .foregroundColor(Color(.sRGB, red: 0.1, green: 0.1, blue: 0.1, opacity: 1)))
+            .cornerRadius(4)
+            .shadow(color: .init(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 5, x: 0, y: 0)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
